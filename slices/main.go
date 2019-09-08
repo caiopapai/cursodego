69091f3a56e0e88b2135a926bff63d35e6728a49
caiopapai <caio_papai@hotmail.com>
2019-09-08 11:18:32 -0300

    aula 2 slices

--- slices/main.go
+++ slices/main.go
@@ -8,9 +8,28 @@ func main() {
 	nomes[0] = "Caio Augusto Papai"
 
 	// Inclui e aumenta o slice
-	nomes = append(nomes, "Outro nome qualquer")
+	nomes[1] = "João da Silda"
+	nomes[2] = "Maria da Silda"
+	nomes[3] = "Paulo da Silda"
+	nomes = append(nomes, "Marco marcola")
 	// Tamanho do Slice
-	fmt.Print(len(nomes))
+	fmt.Println(len(nomes))
 	// Capacidade do Slice
-	fmt.Print(cap(nomes))
+	fmt.Println(cap(nomes))
+	// Pega um intervalo
+	listaDeNomes := nomes[0:5]
+	fmt.Println(listaDeNomes)
+	// Pega pelo começo
+	temp1 := nomes[:2]
+	fmt.Println(temp1)
+	// Pega pelo final
+	temp2 := nomes[len(nomes)-1:]
+	fmt.Println(temp2)
+	// Removendo itens do slice
+	itensARetirar := 2
+	temp := nomes[:itensARetirar]
+	temp = append(temp, nomes[itensARetirar+1:]...)
+	copy(nomes, temp)
+	fmt.Println(nomes)
+
 }
